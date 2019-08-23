eac4ed96267376fe3a2d861fe389601a01360c9d
BitonteGuido <bitonteguido@gmail.com>
2019-08-23 11:30:35 -0300

    Update Python If-Else.py

--- Python If-Else.py
+++ Python If-Else.py
@@ -1,4 +1,13 @@
 #https://www.hackerrank.com/challenges/py-if-else/problem
+import math
+import os
+import random
+import re
+import sys
 
+
+
+if __name__ == '__main__':
+    n = int(input().strip())
     check = {True: "Not Weird", False: "Weird"}
     print(check[n%2==0 and (n in range(2,6) or n > 20)])
