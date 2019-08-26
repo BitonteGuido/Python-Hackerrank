a2b02c86485c18d4c180826b3029dccfe337c4b0
Guido Bitonte <bitonteguido@gmail.com>
2019-08-26 09:01:35 -0300

    Changes

---
+++ 03 Strings/Capitalize!.py
@@ -0,0 +1,13 @@
+def solve(s):
+    return s.title()
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    s = input()
+
+    result = solve(s)
+
+    fptr.write(result + '\n')
+
+    fptr.close()
