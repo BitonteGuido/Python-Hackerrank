7c268b37159e905d9122edf6887c84476566b3a7
BitonteGuido <bitonteguido@gmail.com>
2019-08-26 09:09:22 -0300

    Update Capitalize!.py

--- 03 Strings/Capitalize!.py
+++ 03 Strings/Capitalize!.py
@@ -1,6 +1,6 @@
 def solve(s):
-    return s.title()
-
+    # return s.title() 
+    return ' '.join(i.capitalize() for i in s.split(' '))
 if __name__ == '__main__':
     fptr = open(os.environ['OUTPUT_PATH'], 'w')
 
